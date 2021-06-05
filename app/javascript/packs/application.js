c37688dae2726bc390d9ad1f650cfa078c467ce6
kenzo <kenzou.kenzou104809@gmail.com>
2021-06-05 13:37:35 +0900

    fix: simple_formを使って入力と同時にバリデーションチェックできるようにした。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels");
 require("jquery");
 require("@client-side-validations/client-side-validations");
 require("@client-side-validations/simple-form");
+require("@client-side-validations/simple-form/dist/simple-form.bootstrap4");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
