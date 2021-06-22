ffa6e1ce523f5ae1106a051cb4b5f6abaafd909c
kenzo <kenzou.kenzou104809@gmail.com>
2021-06-22 17:26:03 +0900

    display preview of video input

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,13 @@ require("@client-side-validations/client-side-validations");
 require("@client-side-validations/simple-form");
 require("@client-side-validations/simple-form/dist/simple-form.bootstrap4");
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import $ from "jquery";
+
+$(document).on("turbolinks:load", function () {
+  $("#article_video").on("change", (event) => {
+    const $file = event.currentTarget.files[0];
+    const $video = $(".js-video-player");
+    $video.css("display", "block");
+    $video.attr("src", URL.createObjectURL($file));
+  });
+});
