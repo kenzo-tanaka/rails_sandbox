870ef2e035185a607cecf12505c1ad9d97e774c8
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-08 15:26:12 +0900

    fix: seedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |i|
+  User.create(email: "test#{i}@example.com", password: '123456')
+end
+
+3.times do |i|
+  Staff.create(email: "test#{i}@example.com", password: '123456')
+end
