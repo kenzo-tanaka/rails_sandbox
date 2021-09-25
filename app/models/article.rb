8c087ff8adb0fe94ba9b3855eab3b364d673311d
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-09-25 10:04:06 +0900

    Add ransackable_scopes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   mount_uploader :video, VideoUploader
   mount_uploader :thumbnail, ThumbnailUploader
+
+  scope :created_since, -> (date) {
+    where('created_at >= ?', date)
+  }
+
+  def self.ransackable_scopes(auth_object = nil)
+    %i[created_since]
+  end
 end
