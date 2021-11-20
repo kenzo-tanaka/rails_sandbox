3e3199c60d9c34ece353ee5c802e44252f543286
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-20 16:16:36 +0900

    Revert "Run `rails g scaffold_controller admin/articles --model-name=article --no-helper --no-assets --no-view-specs`"
    This reverts commit 45d850da0fe5e8f4e014b249bdbe065f38f4f65f.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  namespace :admin do
-    resources :articles
-  end
+
   root 'articles#index'
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
