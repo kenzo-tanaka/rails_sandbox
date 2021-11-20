a160038f676c9dbbbada0d6af98d1815a2243781
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-20 16:13:16 +0900

    Create product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_011041) do
+ActiveRecord::Schema.define(version: 2021_11_20_071246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 2021_06_23_011041) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "video"
     t.string "thumbnail"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
