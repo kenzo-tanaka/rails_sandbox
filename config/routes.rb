45d850da0fe5e8f4e014b249bdbe065f38f4f65f
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-20 16:04:33 +0900

    Run `rails g scaffold_controller admin/articles --model-name=article --no-helper --no-assets --no-view-specs`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :articles
+  end
   root 'articles#index'
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
