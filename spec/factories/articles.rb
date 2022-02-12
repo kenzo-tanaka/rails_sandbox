b5281661550a5a6f18f93c34ec566c14a8b82524
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-12 13:33:41 +0900

    chore: factory_bot -> factory_bot_rails

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :article do
-    title "MyString"
+    title { "Mystring" }
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  let!(:articles) { create_list(:article, 10) }
+  let!(:articles) { create(:article) }
 
   it { expect(true).to eq true }
   it { expect(true).to eq true }
