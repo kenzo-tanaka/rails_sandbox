9f9b1bfa3201b0597c2e6b8e5a131bc924629344
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-09-19 13:41:10 +0900

    Display current user

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,33 +12,33 @@ require("@client-side-validations/client-side-validations");
 require("@client-side-validations/simple-form");
 require("@client-side-validations/simple-form/dist/simple-form.bootstrap4");
 
-document.addEventListener("turbolinks:load", () => {
-  const videoInput = document.querySelector(".js-video-input");
-  const video = document.querySelector(".js-video-player");
-  const canvas = document.getElementById("canvasId");
-  const img = document.getElementById("imgId");
-  const captureLink = document.querySelector(".js-capture-thumbnail");
-
-  videoInput.addEventListener("change", (event) => {
-    const file = event.currentTarget.files[0];
-    videoPreview(file, video);
-  });
-
-  captureLink.addEventListener("click", () => {
-    img.style.display = "block";
-    draw(video, canvas, img);
-  });
-
-  const videoPreview = (file, videoTag) => {
-    videoTag.style.display = "block";
-    videoTag.setAttribute("src", URL.createObjectURL(file));
-  };
-
-  // https://developer.mozilla.org/ja/docs/Web/API/Canvas_API/Tutorial/Using_images
-  const draw = (video, canvas, img) => {
-    const context = canvas.getContext("2d");
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const dataUrl = canvas.toDataURL();
-    img.setAttribute("src", dataUrl);
-  };
-});
+// document.addEventListener("turbolinks:load", () => {
+//   const videoInput = document.querySelector(".js-video-input");
+//   const video = document.querySelector(".js-video-player");
+//   const canvas = document.getElementById("canvasId");
+//   const img = document.getElementById("imgId");
+//   const captureLink = document.querySelector(".js-capture-thumbnail");
+//
+//   videoInput.addEventListener("change", (event) => {
+//     const file = event.currentTarget.files[0];
+//     videoPreview(file, video);
+//   });
+//
+//   captureLink.addEventListener("click", () => {
+//     img.style.display = "block";
+//     draw(video, canvas, img);
+//   });
+//
+//   const videoPreview = (file, videoTag) => {
+//     videoTag.style.display = "block";
+//     videoTag.setAttribute("src", URL.createObjectURL(file));
+//   };
+//
+//   // https://developer.mozilla.org/ja/docs/Web/API/Canvas_API/Tutorial/Using_images
+//   const draw = (video, canvas, img) => {
+//     const context = canvas.getContext("2d");
+//     context.drawImage(video, 0, 0, canvas.width, canvas.height);
+//     const dataUrl = canvas.toDataURL();
+//     img.setAttribute("src", dataUrl);
+//   };
+// });
